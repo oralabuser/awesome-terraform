04e23895276ff6580b192d00052a375af0c74d96
oralabuser <82149355+oralabuser@users.noreply.github.com>
2021-11-24 11:07:58 +0530

    Update myInstance.tf

--- myInstance.tf
+++ myInstance.tf
@@ -1,14 +1,14 @@
 provider "aws" {
   region     = "us-east-1"
-  access_key = "<Your Access Key>"
-  secret_key = "<Your Secret Key>"
+  access_key = "AKIAUZBJNUI7GUINNXHR"
+  secret_key = "4RJ6Z2IZURbJGhl4+DCB7OmZ0nG7lXjuCjNpBsLr"
 }
 
-resource "aws_instance" "<Instance_Name>" {
-   ami = "<ami-ID in the Region>"
+resource "aws_instance" "DemoTerraformCloud" {
+   ami = "ami-020001fa39dfd1881"
    instance_type = "t2.micro"
 
    tags = {
-     Name = "<Tag_name>"
+     Name = "DemoTerraformCloud"
    }
 }
